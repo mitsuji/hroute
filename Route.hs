940dd43084e0f9fa75bc670431cd1f45d08dc379
mitsuji <mitsuji@na-s.jp>
2015-03-11 21:58:04 +0900

    creme

--- Route.hs
+++ Route.hs
@@ -1,30 +1,31 @@
--- ×できること論 -> チューリング完全なら同じなので不毛
--- ×効率論 -> 全部アセンブリが一番速いので不毛
--- ○表現力論 
--- ○操作性論
-
 import Prelude hiding( Left, Right )
 import Data.List(foldl')
 
+-- "向き"の型
 data Heading = North
              | East
              | West
              | South
-             -- [POINT] 文字列表現の自動導出
              deriving( Show, Read )
 
+-- 現在の状態の型 ( (X座標, Y座標), "向き" )
 type Status = ( (Int, Int), Heading )
 
+-- 移動方向の型
 data Direction = Forward
                | Backward
                | Left
                | Right
-               -- [POINT] 文字列表現の自動導出
                deriving( Enum, Show, Read )
 
 
--- [POINT] 明確なパターン分け
+--
+-- [New!!] if や switch を使わずにパターンを網羅できる
+--
+
+-- 移動 :: 方向 -> 旧状態 -> 新状態
 advance:: Direction -> Status -> Status
+
 advance Forward  ((x, y), North) = (( x, y+1 ), North)
 advance Backward ((x, y), North) = (( x, y-1 ), South)
 advance Left     ((x, y), North) = (( x-1, y ), West )
@@ -45,133 +46,261 @@ advance Backward ((x, y), South) = (( x, y+1 ), North)
 advance Left     ((x, y), South) = (( x+1, y ), East )
 advance Right    ((x, y), South) = (( x-1, y ), West )
 
-f1 = advance Right (　advance Forward (　advance Left ( advance Forward ( advance Forward ((0,0),North) ) ) ) )
 
 
--- [POINT] 2引数の関数に引数を部分適応して1引数の関数
+-- 移動してみる
+ex1a = advance Forward ((0,0),North)
+-- ((0,1),North)
+
+ex1b = advance Left ((0,0),North)
+-- ((-1,0),West)
+
+
+-- 連続移動してみる( 前 - 前 - 右 - 前 - 左 )
+ex1c = advance Left (　advance Forward (　advance Right ( advance Forward ( advance Forward ((0,0),North) ) ) ) )
+-- ((2,3),North)
+
+
+
+
+-- 連続移動が読みにくいので関数の引数を固定してみる
+
+-- 前進 :: 旧状態 -> 新状態
 forward :: Status -> Status
 forward  = advance Forward
+
+-- 後退 :: 旧状態 -> 新状態
+backward :: Status -> Status
 backward = advance Backward
+
+-- 左折 :: 旧状態 -> 新状態
+left :: Status -> Status
 left = advance Left
+
+-- 右折 :: 旧状態 -> 新状態
+right :: Status -> Status
 right = advance Right
 
-f2 = right ( forward ( left ( forward ( forward ((0,0),North) ) ) ) )
+
+ex2a = forward ((0,0),North)
+-- ((0,1),North)
+
+ex2b = left ((0,0),North)
+-- ((-1,0),West)
 
 
--- [POINT] 演算時を使った関数適応(右から結合)
-f3 = right $ forward $ left $ forward $ forward ((0,0),North)
+-- 少し読みやすくなった( 前 - 前 - 右 - 前 - 左 )
+ex2c = left ( forward ( right ( forward ( forward ((0,0),North) ) ) ) )
+-- ((2,3),North)
 
 
--- [POINT] 引数の順序を入れ替えて演算子として使う(左から結合)
+--
+-- [New!!] 演算子を使って式を評価できるのでカッコが省略できる
+--
+-- さらに読みやすくなった( 前 - 前 - 右 - 前 - 左 )
+ex2d = left $ forward $ right $ forward $ forward ((0,0),North)
+-- ((2,3),North)
+
+
+
+
+-- 引数の順番を入れ替えてみる
+
+-- 移動' :: 旧状態 -> 方向 -> 新状態
 advance':: Status -> Direction -> Status
 advance' = flip advance
 
-f4 = ((0,0),North) `advance'` Forward `advance'` Forward `advance'` Left `advance'` Forward `advance'` Right
+--
+-- [New!!] 関数を演算子として使う
+--
+-- 長くなったけど順番は見やすい( 前 - 前 - 右 - 前 - 左 )
+ex3a = ((0,0),North) `advance'` Forward `advance'` Forward `advance'` Right `advance'` Forward `advance'` Left
+-- ((2,3),North)
 
 
--- [POINT] 独自演算子の定義
+--
+-- [New!!] 独自演算子の定義(オーバーロードではなく定義)
+--
 (==>):: Status -> Direction -> Status
 (==>) = advance'
 
-f5 = ((0,0),North) ==> Forward ==> Forward ==> Left ==> Forward ==> Right
+-- そうとう見やすくなった( 前 - 前 - 右 - 前 - 左 )
+ex3b = ((0,0),North) ==> Forward ==> Forward ==> Right ==> Forward ==> Left
+-- ((2,3),North)
 
 
 -- あるいは
 ($>) = flip ($)
-f6 = ((0,0),North) $> forward $> forward $> left $> forward $> right
+
+-- 同じくらい見やすい( 前 - 前 - 右 - 前 - 左 )
+ex3c = ((0,0),North) $> forward $> forward $> right $> forward $> left
+-- ((2,3),North)
 
 
--- 移動をリストで指定
+
+
+
+-- 移動指示をリストで指定できるようにしてみる
 advances :: Status -> [Direction] -> Status
 advances = foldl' (==>)
 
-f7 = advances ((0,0),North) [Forward, Forward, Left, Forward, Right]
+--
+-- [New!!] こんなに簡単に?
+--
+-- リストで移動指示( 前 - 前 - 右 - 前 - 左 )
+ex4a = advances ((0,0),North) [Forward, Forward, Right, Forward, Left]
+-- ((2,3),North)
 
 
--- 経路探索問題解きたくなってきた
+
+
+-- 経路探索してみたくなった
 permutations' ::  [a] -> Int -> [[a]]
 permutations' = func [[]]
   where
     func acc _ 0 = acc
-    func acc xs i = (func [ (x:ys) | x <- xs, ys <- acc ] xs (i-1)) ++ acc
+    func acc xs i = acc ++ (func [ (x:ys) | x <- xs, ys <- acc ] xs (i-1))
 
--- ある場所にいけるn手以内の全経路（アルゴリズムは力技）
+-- こんな関数を作った
+ex4b = permutations' [1,2,3] 4
+-- [[],[1],[2],[3],[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3],[1,1,1],[1,1,2],[1,1,3],[1,2,1],[1,2,2],[1,2,3],[1,3,1],[1,3,2],[1,3,3],[2,1,1],[2,1,2],[2,1,3],[2,2,1],[2,2,2],[2,2,3],[2,3,1],[2,3,2],[2,3,3],[3,1,1],[3,1,2],[3,1,3],[3,2,1],[3,2,2],[3,2,3],[3,3,1],[3,3,2],[3,3,3],[1,1,1,1],[1,1,1,2],[1,1,1,3],[1,1,2,1],[1,1,2,2],[1,1,2,3],[1,1,3,1],[1,1,3,2],[1,1,3,3],[1,2,1,1],[1,2,1,2],[1,2,1,3],[1,2,2,1],[1,2,2,2],[1,2,2,3],[1,2,3,1],[1,2,3,2],[1,2,3,3],[1,3,1,1],[1,3,1,2],[1,3,1,3],[1,3,2,1],[1,3,2,2],[1,3,2,3],[1,3,3,1],[1,3,3,2],[1,3,3,3],[2,1,1,1],[2,1,1,2],[2,1,1,3],[2,1,2,1],[2,1,2,2],[2,1,2,3],[2,1,3,1],[2,1,3,2],[2,1,3,3],[2,2,1,1],[2,2,1,2],[2,2,1,3],[2,2,2,1],[2,2,2,2],[2,2,2,3],[2,2,3,1],[2,2,3,2],[2,2,3,3],[2,3,1,1],[2,3,1,2],[2,3,1,3],[2,3,2,1],[2,3,2,2],[2,3,2,3],[2,3,3,1],[2,3,3,2],[2,3,3,3],[3,1,1,1],[3,1,1,2],[3,1,1,3],[3,1,2,1],[3,1,2,2],[3,1,2,3],[3,1,3,1],[3,1,3,2],[3,1,3,3],[3,2,1,1],[3,2,1,2],[3,2,1,3],[3,2,2,1],[3,2,2,2],[3,2,2,3],[3,2,3,1],[3,2,3,2],[3,2,3,3],[3,3,1,1],[3,3,1,2],[3,3,1,3],[3,3,2,1],[3,3,2,2],[3,3,2,3],[3,3,3,1],[3,3,3,2],[3,3,3,3]]
+
+
+-- ある場所にいけるn手以内の全経路（アルゴリズムは総当たりです...）
 routes :: Status -> (Int,Int) -> Int -> [[Direction]]
 routes start goal max = filter (\ds-> (advances start ds) `isReachable` goal )
                         $ permutations' [Forward ..] max
   where
     isReachable ((x,y),_) (x',y') = ( x == x' && y == y' )
 
--- (0,0)北向 から (2,4) にいける 7手以内の全経路
-f8 = routes ((0,0),North) (2,3) 7
+
+-- ((0,0),北向) から ((2,3),[向き問わず]) に行ける 10手以内の経路を3つ探索
+ex4c = take 3 $ routes ((0,0),North) (2,3) 10
+-- [[Forward,Forward,Forward,Right,Forward],[Forward,Forward,Right,Forward,Left],[Forward,Forward,Right,Left,Right]]
+
 
 
 
 
 
 --
--- 非決定的な計算への対応(強い型なので)
+-- [New!!] 関数をパワーアップするといろんな計算に対応できる!!
 --
-forward' :: Monad m => Status -> m Status
-forward' = \x -> return $ forward x
-backward' :: Monad m => Status -> m Status
-backward' = \x -> return $ backward x
-left' :: Monad m => Status -> m Status
-left' = \x -> return $ left x
-right' :: Monad m => Status -> m Status
-right' = \x -> return $ right x
+forwardM :: Monad m => Status -> m Status
+forwardM = \st -> return $ forward st
 
-f10 :: Monad m => m Status
-f10 = return((0,0),North) >>= forward' >>= forward' >>= left' >>= forward' >>= right'
+backwardM :: Monad m => Status -> m Status
+backwardM = \st -> return $ backward st
+
+leftM :: Monad m => Status -> m Status
+leftM = \st -> return $ left st
+
+rightM :: Monad m => Status -> m Status
+rightM = \st -> return $ right st
+
+
+-- 今までと同じようにも使える
+ex5a :: Monad m => m Status
+ex5a = return((0,0),North) >>= forwardM >>= forwardM >>= rightM >>= forwardM >>= leftM
+-- ((2,3),North)
 
 
 
+-- ターニングポイントを設置してみる( 前 - 前 - ? - 前 - 左 )
+-- ある地点での計算を引数にとる関数
 go :: Monad m => (Status -> m Status) -> m Status
-go adv = return((0,0),North) >>= forward' >>= forward' >>= adv >>= forward' >>= right'
+go turn = return((0,0),North) >>= forwardM >>= forwardM >>= turn >>= forwardM >>= leftM
 
--- [POINT] 途中で解なしになるかもしれない計算(範囲外など)
-f11 :: Maybe Status
-f12 :: Maybe Status
-f13 :: Maybe Status
-f12 = go $ \st -> Just (left st)
-f11 = go $ \st -> Just (backward st)
-f13 = go $ \st -> Nothing
 
--- [POINT] 解が複数ある計算
-f14 :: [Status]
-f15 :: [Status]
-f16 :: [Status]
-f14 = go $ \st -> [left st,backward st]
-f15 = go $ \st -> [left st]
-f16 = go $ \st -> []
+--
+-- [New!!] 答えがないかもしれない計算（途中で範囲外になったとか）
+--
+ex6a :: Maybe Status
+ex6b :: Maybe Status
+ex6c :: Maybe Status
 
--- [POINT] 外部からの入力で解が変わる計算
-f17 :: IO Status
-f17 = go $ \st -> getLine >>= \line -> return (read line :: Direction) >>= \dir -> return (advance dir st)
+ex6a = go $ \st -> Just (right st)
+-- Just ((2,3),North)
 
-f17' :: IO Status
-f17' = go $ \st-> do
+ex6b = go $ \st -> Just (backward st)
+-- Just ((1,0),East)
+
+-- ターニングポイントで"解なし"になると式全体も"解なし"になる
+ex6c = go $ \st -> Nothing
+-- Nothing
+
+
+
+--
+-- [New!!] 解が複数ある計算（パラメータをリストで与えると結果もリストで返ってくる）
+--
+ex7a :: [Status]
+ex7b :: [Status]
+ex7c :: [Status]
+
+ex7a = go $ \st -> [backward st, right st, left st]
+-- [((1,0),East),((2,3),North),((-2,1),South)]
+
+ex7b = go $ \st -> [right st]
+-- [((2,3),North)]
+
+ex7c = go $ \st -> []
+-- []
+
+
+
+
+getDirection :: IO Direction
+getDirection = getLine >>= \line -> return ( read line :: Direction )
+
+ex8a :: IO Status
+ex8a = go $ do \st -> getDirection >>= \dir -> return (advance dir st)
+-- 入力待ちになるので "Forward" "Backward" "Left" "Right" などを入力
+-- ターニングポイントが入力した移動方向だった時の計算が行われる
+
+-- わかりやすく書くとこう
+ex8b :: IO Status
+ex8b = go $ \st-> do
   line <- getLine
-  return $ advance (read line :: Direction) st
+  let dir = read line :: Direction
+  return (advance dir st)
+
+
+
+
+
 
 
 main :: IO()
 main = do
-  print f1
-  print f2
-  print f3
-  print f4
-  print f5
-  print f6
-  print f7
-  print $ length f8
-  f10 >>= \st -> print st
-  print f11
-  print f12
-  print f13
-  print f14
-  print f15
-  print f16
-  f17 >>= \st -> print st
-  f17' >>= \st -> print st
+
+  print ex1a
+  print ex1b
+  print ex1c
+
+  print ex2a
+  print ex2b
+  print ex2c
+  print ex2d
+
+  print ex3a
+  print ex3b
+  print ex3c
+  
+  print ex4a
+  print ex4b
+  print ex4c
+  
+  print =<< ex5a
+
+  print ex6a
+  print ex6b
+  print ex6c
+  
+  print ex7a
+  print ex7b
+  print ex7c
+
+  print =<< ex8a
+  print =<< ex8b
 
